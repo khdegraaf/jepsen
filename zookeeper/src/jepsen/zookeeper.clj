72a97e67d1c9b3c40d76cfb64e552f9f67985469
Kevin DeGraaf <khdegraaf@gmail.com>
2017-04-14 09:41:45 -0700

    fix version.

--- zookeeper/src/jepsen/zookeeper.clj
+++ zookeeper/src/jepsen/zookeeper.clj
@@ -112,7 +112,7 @@
          opts
          {:name    "zookeeper"
           :os      debian/os
-          :db      (db "3.4.5+dfsg-2")
+          :db      (db "3.4.5+dfsg-2+deb8u")
           :client  (client nil nil)
           :nemesis (nemesis/partition-random-halves)
           :generator (->> (gen/mix [r w cas])
